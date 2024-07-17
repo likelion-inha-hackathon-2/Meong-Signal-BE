b6369e5a637948125fa8aa20c8dda52bc1657a99
sae2say <lollol0617@gmail.com>
2024-07-18 03:21:25 +0900

    feat#4 : 회원정보 수정, 업데이트 구현

--- meong_signal/account/views.py
+++ meong_signal/account/views.py
@@ -2,20 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import update_last_login
 
-import logging
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
+
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 from .models import User
 from .serializer import UserSerializer
 
-logger = logging.getLogger(__name__)
 
 ##########################################
 # api 1 : 회원가입
@@ -60,8 +59,6 @@ def login(request):
     email = request.data.get('email')
     password = request.data.get('password')
 
-    logger.info(f"Attempting login with email: {email}")
-
     user = authenticate(email=email, password=password)
 
 
@@ -76,3 +73,52 @@ def login(request):
 
 ##########################################
 
+##########################################
+# api 3 : 사용자 정보 확인
+
+@swagger_auto_schema(
+    method="GET",
+    tags=["account api"],
+    operation_summary="사용자 정보 확인",
+    responses={
+        200: UserSerializer,
+        401: 'Unauthorized'
+    }
+)
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+def get_user_info(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+##########################################
+
+##########################################
+# api 4 : 사용자 정보 수정
+
+# 수정 가능한 정보 : [주소, 닉네임, 프로필 사진]
+# 수정 불가한 정보 : [이메일, 패스워드, 멍, is_staff, is_superuser, 총 이동거리, 총 소비 칼로리]
+
+@swagger_auto_schema(
+    method="PUT",
+    tags=["account api"],
+    operation_summary="사용자 정보 수정",
+    request_body=UserSerializer,
+    responses={
+        200: UserSerializer,
+        400: 'Bad Request',
+        401: 'Unauthorized'
+    }
+)
+@api_view(['PUT'])
+@authentication_classes([JWTAuthentication])
+def update_user_info(request):
+    user = request.user
+    serializer = UserSerializer(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message':'정상적으로 수정되었습니다.'}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+##########################################
