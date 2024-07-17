02313fe98cd7a06b0d8253785ff352ae9190dd5a
sae2say <lollol0617@gmail.com>
2024-07-18 01:38:38 +0900

    feat #4 - 회원가입, 로그인 구현

--- meong_signal/meong_signal/urls.py
+++ meong_signal/meong_signal/urls.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="meong-signal",
+        default_version='1.1.1',
+        description="감자탕후루 api 문서",
+        terms_of_service="https://www.google.com/policies/terms/"
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('account/', include('account.urls')),
+
+    # Swagger url
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
