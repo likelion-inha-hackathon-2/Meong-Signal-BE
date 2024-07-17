d27c77fefefa732a5c2737fb16ecfef2f16cda53
seaniiio <cnc4750@naver.com>
2024-07-17 22:56:16 +0900

    fix: #3 Review의 FK에 related_name 추가

--- meong_signal/review/models.py
+++ meong_signal/review/models.py
@@ -4,14 +4,14 @@ from dog.models import Dog
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class UserReview(models.Model):
-    evaluated_user_id = models.ForeignKey(User, on_delete=models.CASCADE) # 리뷰받은 사람의 id
-    evaluator_id = models.ForeignKey(User, on_delete=models.CASCADE) # 리뷰남긴 사람의 id
+    evaluated_user_id = models.ForeignKey(User, related_name="user_reviews_received", on_delete=models.CASCADE) # 리뷰받은 사람의 id
+    evaluator_id = models.ForeignKey(User, related_name="user_reviews_given", on_delete=models.CASCADE) # 리뷰남긴 사람의 id
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     content = models.CharField(max_length=50)
 
 class WalkingReview(models.Model):
-    evaluated_user_id = models.ForeignKey(User, on_delete=models.CASCADE) # 리뷰받은 사람의 id
-    evaluator_id = models.ForeignKey(User, on_delete=models.CASCADE) # 리뷰남긴 사람의 id
+    evaluated_user_id = models.ForeignKey(User, related_name="walk_reviews_received", on_delete=models.CASCADE) # 리뷰받은 사람의 id
+    evaluator_id = models.ForeignKey(User, related_name="walk_reviews_given", on_delete=models.CASCADE) # 리뷰남긴 사람의 id
     dog_id = models.ForeignKey(Dog, on_delete=models.CASCADE) # 같이 산책한 강아지의 id
     content = models.CharField(max_length=50)
 
