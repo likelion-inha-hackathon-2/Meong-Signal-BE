9c49e7820c3e5f19164cbd67107e30f8270a3409
seaniiio <cnc4750@naver.com>
2024-07-18 03:23:17 +0900

    feat: #3 회원가입 시 프로필사진 받아오게 구현

--- meong_signal/meong_signal/urls.py
+++ meong_signal/meong_signal/urls.py
@@ -8,22 +8,26 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="meong-signal",
-        default_version='1.1.1',
-        description="감자탕후루 api 문서",
-        terms_of_service="https://www.google.com/policies/terms/"
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="meong-signal",
+#         default_version='1.1.1',
+#         description="감자탕후루 api 문서",
+#         terms_of_service="https://www.google.com/policies/terms/"
+#     ),
+#     public=True,
+#     permission_classes=(AllowAny,),
+# )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 
     # Swagger url
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
+
+
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) -> swagger관련 설정
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
