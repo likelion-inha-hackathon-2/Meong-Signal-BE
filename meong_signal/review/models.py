233c3d0ffe2630f4e5c5b1456761c1c94ff049e6
seaniiio <cnc4750@naver.com>
2024-07-20 21:41:18 +0900

    feat: #27 Review 모델 수정

--- meong_signal/review/models.py
+++ meong_signal/review/models.py
@@ -2,29 +2,62 @@ from django.db import models
 from account.models import User
 from dog.models import Dog
 from django.core.validators import MinValueValidator, MaxValueValidator
+from walk.models import Walk
 
 class UserReview(models.Model):
-    evaluated_user_id = models.ForeignKey(User, related_name="user_reviews_received", on_delete=models.CASCADE) # 리뷰받은 사람의 id
-    evaluator_id = models.ForeignKey(User, related_name="user_reviews_given", on_delete=models.CASCADE) # 리뷰남긴 사람의 id
+    walk_id = models.OneToOneField(Walk, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     content = models.CharField(max_length=50)
 
+    @property
+    def user(self): # user: 산책 한 장본인 -> 리뷰 받은 사람
+        return self.walk_id.user_id
+    
+    @property
+    def owner(self): # owner: 강아지 빌려준 견주 -> 리뷰 남긴 사람
+        return self.walk_id.owner_id
+    
+    @property
+    def dog(self): # 같이 산책한 강아지
+        return self.walk_id.dog_id
+
     def __str__(self):
-        r = f'리뷰 : 견주 id {self.evaluator_id} -> 산책자 id {self.evaluated_user_id}'
+        r = f'리뷰 : 견주 id {self.owner} -> 산책자 id {self.owner}'
         return r
 
 class WalkingReview(models.Model):
-    evaluated_user_id = models.ForeignKey(User, related_name="walk_reviews_received", on_delete=models.CASCADE) # 리뷰받은 사람의 id
-    evaluator_id = models.ForeignKey(User, related_name="walk_reviews_given", on_delete=models.CASCADE) # 리뷰남긴 사람의 id
-    dog_id = models.ForeignKey(Dog, on_delete=models.CASCADE) # 같이 산책한 강아지의 id
+    walk_id = models.OneToOneField(Walk, on_delete=models.CASCADE)
     content = models.CharField(max_length=50)
 
+    @property
+    def user(self): # user: 산책 한 장본인 -> 리뷰 남긴 사람
+        return self.walk_id.user_id
+    
+    @property
+    def owner(self): # owner: 강아지 빌려준 견주 -> 리뷰 받은 사람
+        return self.walk_id.owner_id
+    
+    @property
+    def dog(self): # 같이 산책한 강아지
+        return self.walk_id.dog_id
+
     def __str__(self):
-        r = f'산책 리뷰 : 산책자 id {self.evaluator_id} -> 견주 id {self.evaluated_user_id}'
+        r = f'산책 리뷰 : 산책자 id {self.user} -> 견주 id {self.owner}'
         return r
 
 
 class ReviewTag(models.Model):
     review_id = models.ForeignKey(WalkingReview, on_delete=models.CASCADE)
-    dog_id = models.ForeignKey(Dog, on_delete=models.CASCADE) # 같이 산책한 강아지의 id
     number = models.IntegerField()
+
+    @property
+    def owner(self): # owner: 강아지 빌려준 견주
+        return self.review_id.owner
+    
+    @property
+    def dog(self): # 같이 산책한 강아지
+        return self.review_id.dog
+    
+    def __str__(self):
+        r = f'강아지 id {self.dog} -> 태그 번호 {self.number}'
+        return r
