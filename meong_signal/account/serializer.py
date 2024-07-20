91fd03302707221ce39fd383545e3827d4093cab
seaniiio <cnc4750@naver.com>
2024-07-20 13:28:48 +0900

    feat: #25 회원가입 시 프로필사진 등록 기능 구현

--- meong_signal/account/serializer.py
+++ meong_signal/account/serializer.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import *
+import uuid
+from django.core.files.base import ContentFile
+
+def generate_uuid_filename(extension):
+    unique_id = uuid.uuid4().hex
+    return f'{unique_id}.{extension}'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +13,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        if 'profile_image' in validated_data:
+            # 파일의 확장자 추출
+            image = validated_data['profile_image']
+            file_extension = image.name.split('.')[-1]
+            
+            # UUID를 사용한 새 파일 이름 생성
+            new_file_name = generate_uuid_filename(file_extension)
+
+            # 파일을 메모리에 저장
+            temp_file = ContentFile(image.read())
+            temp_file.name = new_file_name
+
+            
         user = User(
             email=validated_data['email'],
             nickname=validated_data['nickname'],
@@ -15,8 +34,9 @@ class UserSerializer(serializers.ModelSerializer):
             meong=validated_data.get('meong', 0),
             total_distance=validated_data.get('total_distance', 0),
             total_kilocalories=validated_data.get('total_kilocalories', 0),
-            profile_image=validated_data.get('profile_image', '')
+            profile_image = temp_file
         )
+
         user.set_password(validated_data['password'])
         user.save()
         return user
