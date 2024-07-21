f1f90bd64be271a9a12e85b163808c1bba7cdb46
sae2say <lollol0617@gmail.com>
2024-07-22 03:28:30 +0900

    채팅 전송 확인

--- meong_signal/chat/consumers.py
+++ meong_signal/chat/consumers.py
@@ -1,36 +1,65 @@
-# chat/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth.models import AnonymousUser
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = f'chat_{self.room_id}'
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        # JWT token authentication
+        query_string = self.scope['query_string'].decode()
+        print(f"Query string: {query_string}")  # Query string 값을 출력하여 확인
 
-        await self.accept()
+        token_key, token_value = query_string.split('=', 1)
+        print(f"Token key: {token_key}, Token value: {token_value}")  # Token key와 value 값을 출력하여 확인
+
+        if token_key != 'token':
+            print("Invalid token key")
+            await self.close()
+            return
+        
+        self.user = await self.get_user_from_token(token_value)
+
+        if self.user.is_anonymous:
+            print("Anonymous user")
+            await self.close()
+        else:
+            try:
+                print('self.channel_layer:', self.channel_layer)
+                print('self.channel_name:', self.channel_name)
+                await self.channel_layer.group_add(
+                    self.room_group_name,
+                    self.channel_name
+                )
+                print('Added to group')
+                await self.accept()
+            except Exception as e:
+                print(f"Error during group add or accept: {e}")
+                await self.close()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+        await super().disconnect(close_code)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        sender = self.scope["user"]
+        sender = self.user
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'sender': sender.username,
+                'sender': sender.nickname,
             }
         )
 
@@ -42,3 +71,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
             'sender': sender,
         }))
+
+    @database_sync_to_async
+    def get_user_from_token(self, token):
+        try:
+            # Validate the token
+            UntypedToken(token)
+            # If token is valid, get the user from the token
+            jwt_authenticator = JWTAuthentication()
+            validated_token = jwt_authenticator.get_validated_token(token)
+            user = jwt_authenticator.get_user(validated_token)
+            return user
+        except (InvalidToken, TokenError) as e:
+            print(f"Token error: {str(e)}")
+            return AnonymousUser()
--- meong_signal/meong_signal/asgi.py
+++ meong_signal/meong_signal/asgi.py
@@ -11,12 +11,14 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meong_signal.settings')
+django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
