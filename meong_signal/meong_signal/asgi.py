eab7eb3cf32bad6c203e9b41bf60bd28f1912ed8
sae2say <lollol0617@gmail.com>
2024-07-22 18:17:54 +0900

    fix-re-#31 : 주석처리

--- meong_signal/meong_signal/asgi.py
+++ meong_signal/meong_signal/asgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+#from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meong_signal.settings')
 
@@ -19,7 +19,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            #websocket_urlpatterns
         )
     ),
 })
