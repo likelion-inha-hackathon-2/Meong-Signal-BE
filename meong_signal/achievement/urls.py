0fc5e89ac53c7d583dc7c1c4a464a46989768187
seaniiio <cnc4750@naver.com>
2024-07-22 16:22:21 +0900

    feat: #6 유저와 관련된 업적 목록 조회 api 구현

--- meong_signal/achievement/urls.py
+++ meong_signal/achievement/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('new', views.new_achievement),
+    path('all', views.get_achievement)
 ]
--- meong_signal/achievement/views.py
+++ meong_signal/achievement/views.py
@@ -26,7 +26,6 @@ from geopy.distance import geodesic
 
 # Create your views here.
 
-
 @swagger_auto_schema(
     method="POST",
     tags=["achievement api"],
@@ -43,3 +42,35 @@ def new_achievement(request):
         serializer.save()
         return Response(serializer.data, status=200)
     return Response(serializer.errors, status=400)
+
+@swagger_auto_schema(
+    method="GET",
+    tags=["achievement api"],
+    operation_summary="업적 조회 api",
+)
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+def get_achievement(request):
+    user_id = request.user.id
+    return_data = {"dog":[], "walking":[]}
+
+    achievements = UserAchievement.objects.filter(user_id = user_id)
+    for achievement in achievements:
+        achievement_data = {}
+        try:
+            original_achievement = Achievement.objects.get(id = achievement.achievement_id.id)
+            print("original_achievement:", original_achievement)
+        except ObjectDoesNotExist:
+            return Response({"error" : "User에 관한 Achievement를 찾을 수 없습니다."}, status=400)
+
+        category = original_achievement.category
+        achievement_data["title"] = original_achievement.title
+        achievement_data["total_count"] = float(original_achievement.total_count)
+        achievement_data["now_count"] = float(achievement.count)
+        achievement_data["is_representative"] = float(achievement.is_representative)
+        achievement_data["is_achieved"] = float(achievement.is_achieved)
+        achievement_data["id"] = achievement.id
+
+        return_data[category].append(achievement_data)
+
+    return Response(return_data, status=200)
