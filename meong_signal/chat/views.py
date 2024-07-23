9fb60ae343af2fd325e961cf049b79aa6dd7c76e
sae2say <lollol0617@gmail.com>
2024-07-23 19:41:46 +0900

    히히커밋

--- meong_signal/chat/views.py
+++ meong_signal/chat/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
