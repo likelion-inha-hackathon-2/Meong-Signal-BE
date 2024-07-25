cecba70b5133a0c835b17137ecf521f6e1c2061b
seaniiio <cnc4750@naver.com>
2024-07-26 04:13:19 +0900

    fix: settings.py channels 주석처리

--- meong_signal/meong_signal/settings/base.py
+++ meong_signal/meong_signal/settings/base.py
@@ -270,14 +270,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [(('15.164.185.48',"127.0.0.1"), 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [(('15.164.185.48',"127.0.0.1"), 6379)],
+#         },
+#     },
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
