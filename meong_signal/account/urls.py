bffceddf5ff225ef9d9e84955592316ff3d61026
seaniiio <cnc4750@naver.com>
2024-07-26 18:21:48 +0900

    fix: account 앱 url 수정

--- meong_signal/account/urls.py
+++ meong_signal/account/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('login/kakao/callback', views.kakao_login_callback),
     path('login/naver/callback', views.naver_login_callback),
     path('login/google/callback', views.google_login_callback),
-    path('my-email', views.get_user_email),
 ]
