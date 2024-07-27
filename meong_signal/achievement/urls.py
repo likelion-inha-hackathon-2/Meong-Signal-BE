7b85f0c28eee0ad4452b0324f760f761121f7f57
seaniiio <cnc4750@naver.com>
2024-07-27 22:38:53 +0900

    fix: achievements url 수정

--- meong_signal/achievement/urls.py
+++ meong_signal/achievement/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('new', views.new_achievement),
     path('all', views.get_achievement),
     path('user', views.new_achievement_about_user),
-    path('achievements/set-represent', views.set_representative),
-    path('achievements/represent', views.get_represent),
+    path('set-represent', views.set_representative),
+    path('represent', views.get_represent),
 ]
