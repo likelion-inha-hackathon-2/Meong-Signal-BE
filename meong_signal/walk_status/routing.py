93b1c511e7a9de777cd2789ffb3f53f209e4f99a
seaniiio <cnc4750@naver.com>
2024-07-29 01:20:09 +0900

    fix: 위치 추적 room api url 수정

--- meong_signal/walk_status/routing.py
+++ meong_signal/walk_status/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from walk_status import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_id>\d+)/locations$', consumers.LocationConsumer.as_asgi()),
+    re_path(r'ws/walkroom/(?P<room_id>\d+)/locations$', consumers.LocationConsumer.as_asgi()),
 ]
